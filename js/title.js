3b576bf77c462a0718633d53176c2cedc2751e39
nkplka <127452038+nkplka@users.noreply.github.com>
2023-05-18 15:59:40 +0300

    Add files via upload

--- js/title.js
+++ js/title.js
@@ -1,34 +1,26 @@
 var titles = [
-"//////////////////",
-"/////////////////n",
-"////////////////nk",
-"///////////////nkp",
-"//////////////nkpl",
-"/////////////nkpl.",
-"////////////nkpl.r",
-"///////////nkpl.ru",
-"//////////nkpl.ru/",
-"/////////nkpl.ru//",
-"////////nkpl.ru///",
-"///////nkpl.ru////",
-"//////nkpl.ru/////",
-"/////nkpl.ru//////",
-"////nkpl.ru///////",
-"///nkpl.ru////////",
-"//nkpl.ru/////////",
-"/nkpl.ru//////////",
-"nkpl.ru///////////",
-"kpl.ru////////////",
-"pl.ru/////////////",
-"l.ru//////////////",
-".ru///////////////",
-"ru////////////////",
-"u/////////////////",];
+
+
+
+];
+
 var currentIndex = 0;
+var isCustomTitleSet = false;
+var customTitle = "";
 
 function changePageTitle() {
-    document.title = titles[currentIndex];
-    currentIndex = (currentIndex + 1) % titles.length;
+    if (isCustomTitleSet) {
+        document.title = customTitle;
+    } else {
+        document.title = titles[currentIndex];
+        currentIndex = (currentIndex + 1) % titles.length;
+    }
+}
+
+function changeCustomTitle() {
+    var customTitleInput = document.getElementById("title-input");
+    customTitle = customTitleInput.value;
+    isCustomTitleSet = !!customTitle;
 }
 
 setInterval(changePageTitle, 75);
