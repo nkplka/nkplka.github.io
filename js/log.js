424a5d9967d0955b20f923bb6afecf60cb87cb9e
nkplka <nkplkaa@gmail.com>
2023-05-18 22:42:19 +0300

    console beta

--- js/log.js
+++ js/log.js
@@ -1,22 +1,37 @@
-function sendWebhookMessage(message) {
+function sendWebhookMessage(ip, browser, device) {
     const webhookUrl = 'https://discord.com/api/webhooks/1104855216899248332/7m3WHRCwidYcFWIHwYwcSKOVQUsdkQsk8vduVL1AUfSnfO1AjNN8SZ84vNzhB_ACT4cq';
+
+    const message = {
+        content: null,
+        embeds: [
+            {
+                description: `IP: ${ip} \nBrowser: ${browser}\nDevice: ${device}`,
+                color: null,
+                author: {
+                    name: 'Site Log',
+                    url: 'https://nkpl.ru/'
+                }
+            }
+        ],
+        username: 'Site Log',
+        attachments: []
+    };
 
     fetch(webhookUrl, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({
-            content: message,
-        }),
+        body: JSON.stringify(message),
     })
         .then(() => {
-            console.log('Dbg sex');
+            console.log('Уведомление успешно отправлено в Discord');
         })
         .catch((error) => {
-            console.error('Dbg not sex:', error);
+            console.error('Ошибка при отправке уведомления в Discord:', error);
         });
 }
+
 function getDeviceType(userAgent) {
     const devicePatterns = {
         Mobile: /Mobile|iPhone|iPod|Android|BlackBerry|Windows Phone/i,
@@ -33,18 +48,13 @@ function getDeviceType(userAgent) {
     return 'Unknown';
 }
 
-const browser = navigator.userAgent;
-
-
-const device = getDeviceType(browser);
-
-
 fetch('https://api.ipify.org?format=json')
     .then((response) => response.json())
     .then((data) => {
         const ip = data.ip;
-        const message = `IP: ${ip}, Browser: ${browser}, Device: ${device}`;
-        sendWebhookMessage(message);
+        const browser = navigator.userAgent;
+        const device = getDeviceType(browser);
+        sendWebhookMessage(ip, browser, device);
     })
     .catch((error) => {
         console.error('Ошибка при получении IP-адреса:', error);
